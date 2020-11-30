d936ac9ac7faae18935a8943fa5ae0f3023c246c
Chaojun Liu <chaojunl@microsoft.com>
2020-11-29 17:45:15 -0800

    Set up sign in

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -19,3 +20,16 @@ def index():
 
     bob = { "bob": "hello"}
     return render_template('index.html', title="Home", user=user, posts=posts, bob=bob)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+
+    # print("Bob", form)
+    # print("Form's password method", form.password())
+    # print(form.password(size=54))
+    return render_template('login.html', title='Sign In', form=form)
