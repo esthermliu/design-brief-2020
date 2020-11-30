041b8d6a800ddeb906eed00656f45b24d032e8c7
Chaojun Liu <chaojunl@microsoft.com>
2020-11-29 16:31:40 -0800

    Created base and index

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=webmaster.py
+FLASK_ENV=development
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = { 'username': 'Bob'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': "Beautiful day in Kirkland, Washington!"
+        },
+
+        {
+            'author': {'username': 'Susan'},
+            'body': "My name is Susan!"
+        }
+    ]
+
+    bob = { "bob": "hello"}
+    return render_template('index.html', title="Home", user=user, posts=posts, bob=bob)
