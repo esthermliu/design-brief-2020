4111f244b54136fa5bfbb67aa6a477a4e77b04f3
esthermliu <esther.en.liu@gmail.com>
2021-02-12 19:01:34 -0800

    style changes to rooms

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_moment import Moment
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +13,8 @@ migrate = Migrate(app, db, render_as_batch=True)
 login = LoginManager(app)
 login.login_view = 'login'
 moment = Moment(app)
+mail = Mail(app)
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
